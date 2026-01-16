4745292ee9b62572ffa6f9a66f9d17b456dd5b89
Tofunmi-code11gk <tainabadejo@gmail.com>
2026-01-16 09:55:56 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+
+# Load trained model
+model = load_model("model.h5", compile=False)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    area = float(request.form["area"])
+    bedrooms = float(request.form["bedrooms"])
+    bathrooms = float(request.form["bathrooms"])
+    age = float(request.form["age"])
+
+    features = np.array([[area, bedrooms, bathrooms, age]])
+    prediction = model.predict(features)
+
+    price = round(prediction[0][0], 2)
+
+    return render_template("index.html", prediction=price)
+
+if __name__ == "__main__":
+    app.run(debug=True)
